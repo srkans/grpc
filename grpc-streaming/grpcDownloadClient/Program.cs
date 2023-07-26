04a940ce9b496702f279fe173c9d150c16c3c7b1
srkans <serkansacma@hotmail.com>
2023-07-26 22:24:35 +0300

    file download client

--- grpc-streaming/grpcDownloadClient/Program.cs
+++ grpc-streaming/grpcDownloadClient/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using grpcFileTransferClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7269");
+
+var client = new FileService.FileServiceClient(channel);
+
+string downloadPath = @"F:\grpc\grpc-streaming\grpcDownloadClient\downloads";
+
+var fileInfo = new grpcFileTransferClient.FileInfo
+{
+    FileExtension = ".pdf",
+    FileName = "Architecting-Modern-Web-Applications-with-ASP.NET-Core-and-Azure",
+};
+
+FileStream fileStream = null;
+
+var request = client.FileDownload(fileInfo);
+
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+int count = 0;
+decimal chunkSize = 0;
+
+while(await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+{
+    if(count++ == 0)
+    {
+        fileStream = new FileStream(@$"{downloadPath}\{request.ResponseStream.Current.Info.FileName}{request.ResponseStream.Current.Info.FileExtension}", FileMode.CreateNew);
+
+        fileStream.SetLength(request.ResponseStream.Current.FileSize);
+    }
+
+    var buffer = request.ResponseStream.Current.Buffer.ToByteArray();
+
+    await fileStream.WriteAsync(buffer, 0, request.ResponseStream.Current.ReadedByte);
+
+    Console.WriteLine($"{Math.Round(((chunkSize += request.ResponseStream.Current.ReadedByte) *100 )/request.ResponseStream.Current.FileSize)}%");
+}
+
+Console.WriteLine("Yuklendi...");
+
+await fileStream.DisposeAsync();
+fileStream.Close();
