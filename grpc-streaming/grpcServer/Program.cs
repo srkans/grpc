2171627b79ca2e4cf11deec5c9291f559d619df5
srkans <serkansacma@hotmail.com>
2023-07-26 13:42:16 +0300

    grpc file streaming

---
+++ grpc-streaming/grpcServer/Program.cs
@@ -0,0 +1,30 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// Configure the HTTP request pipeline.
+
+//app.MapGrpcService<GreeterService>();
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
